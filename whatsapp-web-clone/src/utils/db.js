793b5239af4bf4851eb6b8b97dbbff48c9f083a3
faheemw09 <faheemh712@gmail.com>
2024-12-25 21:16:19 +0530

    added the assignment

--- whatsapp-web-clone/src/utils/db.js
+++ whatsapp-web-clone/src/utils/db.js
@@ -1,5 +1,7 @@
 import { init, i, id } from "@instantdb/core";
 
+const APP_ID = "f453ba05-8970-4461-8f29-c38ee658d9e9";
+
 export const schema = i.schema({
   entities: {
     users: i.entity({
@@ -18,7 +20,6 @@ export const schema = i.schema({
 });
 
 // Initialize database
-const APP_ID = process.env.REACT_APP_APP_ID;
 export const db = init({ appId: APP_ID, schema });
 // console.log("Database object:", db);
 console.log("DB Methods", db);
